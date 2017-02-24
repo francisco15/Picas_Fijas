c06cc428d3e2402bc07bed1024a26f28620d503b
francisco15 <franciscohuertas38@gmail.com>
2017-02-24 10:11:15 -0500

    agredado funcion reiniciar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // funcion que genera un Array de numeros aleatorios
 function generateNumber(len) {
     var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    nums.sort(function(){return Math.random() - 0.5});
+    nums.sort(function(){return Math.random() - 0.2});
     return nums.slice(0, len);
 }
 // funcion que verifica que no haya numeros repetidos
@@ -25,27 +25,40 @@ function game(num, guess) {
     }
     return count;
 }
-// genera el numero aleatorio
-var num = generateNumber(4);
-console.log(num.join(""));
-round = 0;
-// funcion que permite al usuario jugar
-$("#userGuess").keypress(function(e){
-    if (e.which == 13) {
-        var guess = Array.from($(this).val());
-        var arrayOfNumbers = guess.map(Number);
-        var play = game(num, arrayOfNumbers);
-        round = round + 1;
-        if ((!(guess.length == 4)) || hasDuplicates(arrayOfNumbers)) {
-        	$(".alert").removeClass("hidden");
-        }
-        else {
-          $(".alert").addClass("hidden");
-          $(".table").removeClass("hidden");
-          $("tbody").append("<tr><td>"+arrayOfNumbers.join("")+"</td><td>"+play.picas+"</td><td>"+play.fijas+"</td></tr>");
-          if (play.fijas == 4) {
-            $(".results").prepend("<p class=winner >¡GANASTE EN <span class=numGuesses >"+round+"</span> INTENTOS!</p>");
+
+$(document).ready(function(){
+  // genera el numero aleatorio
+  num = generateNumber(4);
+  console.log(num.join(""));
+  // reinicio del juego
+  $(".button").on("click", function(e){
+      $("tbody").empty();
+      num = generateNumber(4);
+      console.log(num.join(""));
+  })
+
+  $('#userGuess').keypress(function(e){
+      if (e.which == 13) {
+          var guess = Array.from($(this).val());
+          $(this).val("");
+          var arrayOfNumbers = guess.map(Number);
+          var result = game(num, arrayOfNumbers);
+
+          if ((!(guess.length == 4)) || hasDuplicates(arrayOfNumbers) || isNaN(arrayOfNumbers.join("")) == true) {
+          	$(".alert").removeClass("hidden");
           }
-        }
-    }
-})
+          else {
+            if (result.fijas != 4){
+              $(".alert").addClass("hidden");
+              $(".table").removeClass("hidden");
+              $("tbody").append("<tr><td>"+arrayOfNumbers.join("")+"</td><td>"+result.picas+"</td><td>"+result.fijas+"</td></tr>");
+            }
+            else{
+              $('#modal').modal({
+                  show: true
+              });
+            }
+          }
+      }
+  })
+});
